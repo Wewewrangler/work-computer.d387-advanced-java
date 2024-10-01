7b182b2dd2a2031480aeb80de5f0c57f8bae72c6
nickg <ngre267@wgu.edu>
2024-10-01 16:26:08 -0400

    finished with correctly filling out the MessageController

--- src/main/java/edu/wgu/d387_sample_code/controller/MessageController.java
+++ src/main/java/edu/wgu/d387_sample_code/controller/MessageController.java
@@ -4,6 +4,8 @@ package edu.wgu.d387_sample_code.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 //MIGHT HAVE TO REMOVE "/mainscreen"
 @CrossOrigin("http://localhost:8080/mainscreen")
 @RestController
@@ -17,23 +19,10 @@ public class MessageController {
         this.messageService = messageService;
     }
 
-    /*************************************
-     *              THINGS THAT NEED REPLACING
-     *            1. PurchaseResponse
-     *            2. placeOrder
-     *            3. Purchase
-     *            4. purchase
-     *            5. placeOrder
-     *            6.purchaseResponse
-     *************************************/
 
-
-     //todo ADD THE CORRECT PATH FOR "POSTMAPPING"
-    @PostMapping("/welcome")
-    public PurchaseResponse placeOrder(@RequestBody Purchase purchase) {
-
-        PurchaseResponse purchaseResponse messageService.placeOrder(purchase);
-
-        return purchaseResponse;
+    @GetMapping("/welcome")
+    public List<String> getMessage() {
+        // uses RECURSION to call the service to retrieve messages as a JSON array
+        return messageService.getMessage();
     }
 }
