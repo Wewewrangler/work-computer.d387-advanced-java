6f42326d9da1dad8d2a43155822d9a8ef7f978ef
nickg <ngre267@wgu.edu>
2024-10-08 08:26:01 -0400

    set up the MessageMapping class to the professors specifications

--- src/main/java/edu/wgu/d387_sample_code/rest/MessageMapping.java
+++ src/main/java/edu/wgu/d387_sample_code/rest/MessageMapping.java
@@ -1,4 +1,17 @@
+//FROM PROFESSOR TIMESTAMP 6:40
 package edu.wgu.d387_sample_code.rest;
 
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.MediaType;
+
+@RestController
+@CrossOrigin
+@RequestMapping(“/api”)
 public class MessageMapping {
+
+    @RequestMapping(path = “/presentation”, method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public String[] showPresentation() {
+        String finalMessage = “third message example”;
+        return new String[] {finalMessage};
+    }
 }
